d1483747248f2294bc33ce871175b6bba6b8ad93
Hector David Jimenez Armas <65544852+HectorJimenezA@users.noreply.github.com>
2022-12-07 19:18:21 -0500

    cmabios
    cambios

--- index.php
+++ index.php
@@ -15,50 +15,68 @@
 	<?php
 	include('header.php');
 	?>
-	
+
 	<div class="container">
 		<div class="row">
-
 			<div class="col-md-12">
-
 				<?php include('pelicula_sidebar.php'); ?>
 			</div>
 		</div>
 	</div>
 
-	<br><br>
-	
-	<div class="product_main">
-		<?php
-		$qry3 = mysqli_query($con, "SELECT * FROM tbl_news LIMIT 5");
-		while ($n = mysqli_fetch_array($qry3)) {
-		?>
+
+	<div class="news">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="titlepage">
+						<h2>Estrenos</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="product_main">
 			<div class="project_box ">
 				<div class="dark_white_bg">
+
 					<div class="wrap">
-						<div class="tarjeta-wrap">
-							<div class="tarjeta">
-								<div class="adelante card1">
-									<img width="200%" height="150%"  src="admin/<?php echo $n['attachment']; ?>">
-								</div>
-								<div class="atras">
-									
+						<?php
+						$qry3 = mysqli_query($con, "SELECT * FROM tbl_news LIMIT 5");
+						while ($n = mysqli_fetch_array($qry3)) {
+						?>
+							<div class="tarjeta-wrap">
+								<div class="tarjeta">
+									<div class="adelante card1">
+										<img width="200%" height="150%" src="admin/<?php echo $n['attachment']; ?>">
+									</div>
+									<div class="atras">
 										<span style="text-color:#000" class="data"><strong><?php echo $n['name']; ?></strong><br>
 											<span style="text-color:#000" class="data"><strong>Actores :<?php echo $n['cast']; ?></strong><br>
 												<div class="data">Fecha de Estreno :<?php echo $n['news_date']; ?></div>
 
 												<span class="text-top"><?php echo $n['descripcion']; ?></span>
-						
+
+									</div>
 								</div>
 							</div>
-						</div>
-					<?php
-				}
-					?>
+						<?php
+						}
+						?>
+					</div>
 				</div>
 			</div>
+		</div>
 	</div>
-	</div>
+
+	<br><br>
+	<br><br>
+	<br><br>
+	<br><br>
+	<br><br>
+	<br><br>
+
+
+
 	<footer>
 		<?php include('footer.php'); ?>
 	</footer>
